af6cd4f5ccccd63db1d70a4371434dc6a232161b
Victoria Fakunle <v.fakunle@alustudent.com>
2024-03-24 15:09:14 +0000

    final

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""
-Fetches https://alu-intranet.hbtn.io/status and prints the response.
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
 
-    # Create a request object
-    with urllib.request.urlopen(url) as response:
-        # Read the response content
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-
-        # Decode the content to UTF-8
-        utf8_content = content.decode('utf-8')
-
-        # Display response
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(utf8_content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
