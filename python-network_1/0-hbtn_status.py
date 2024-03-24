680df3205f352041d2d9924038773183ed20a919
Victoria Fakunle <v.fakunle@alustudent.com>
2024-03-24 15:40:37 +0000

    fff

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-"""
-Fetches https://alu-intranet.hbtn.io/status and http://0.0.0.0:5050/status using urllib.
-Displays the body of the response in the specified format.
-"""
+"""  fetches https://intranet.hbtn.io/status  """
 import urllib.request
-import urllib.error
 
-if __name__ == '__main__':
-    urls = ['https://alu-intranet.hbtn.io/status', 'http://0.0.0.0:5050/status']
-    
-    for url in urls:
-        try:
-            with urllib.request.urlopen(url) as response:
-                content = response.read()
-                utf8_content = content.decode('utf-8')
 
-                print("Body response:")
-                print("\t- type: {}".format(type(content)))
-                print("\t- content: {}".format(utf8_content))
-        except urllib.error.URLError as e:
-            print("Error:", e.reason)
-
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
