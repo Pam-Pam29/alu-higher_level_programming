2740625d1779e8dc1ea3cfa0db9a26c1d02a3308
Victoria Fakunle <v.fakunle@alustudent.com>
2024-06-03 13:12:54 +0200

    Update 4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length < 2) {
-  console.log('Two arguments required');
+
+if (args.length === 0) {
+  console.log(`undefined is undefined`);
+} else if (args.length === 1) {
+  console.log(`${args[0]} is undefined`);
 } else {
-  console.log('{args[0]} is {args[1]}');
+  console.log(`${args[0]} is ${args[1]}`);
 }
