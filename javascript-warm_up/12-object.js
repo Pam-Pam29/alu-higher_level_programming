c8b68be8a12a4e36e6ffcecc8e1feea64d798492
Victoria Fakunle <v.fakunle@alustudent.com>
2024-06-06 16:02:35 +0200

    Update 12-object.js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-function replaceValue (args) {
-  if (args.length === 0) {
-    console.log(0);
-  } else {
-    for (let i = 0; i < args.length; i++) {
-      const num = parseInt(args[i]);
-      if (args[i] === '12') {
-        args[i] = '89';
-      }
-    }
-    console.log(args.join(' '));
-  }
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
 
-replaceValue(process.argv.slice(2));
+myObject.value = 89;
+
+console.log(myObject);
