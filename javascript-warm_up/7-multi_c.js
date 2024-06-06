863f31592301abe3afe5d5316dcbfcb37ab29495
Victoria <v.fakunle@alustudent.com>
2024-06-06 05:43:11 -0700

    commit

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+ #!/usr/bin/node
+const language = ['C is fun']
+let i = 0;
+ while (i <= 10) {
+	 console.log(i);
+	 i++;
+ } else { 
+	 console.log('Missing number of occurrences');
+ }
+~         
