d7c21116be9292fcb3bcac8b8461604ec5fcebac
Victoria Fakunle <v.fakunle@alustudent.com>
2024-06-06 15:21:53 +0200

    Update 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-const arg = process.argv[2];
 function sum (a, b) {
   const result = a + b;
   console.log(result);
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
 }
