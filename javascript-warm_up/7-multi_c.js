860cb0a83267ee9deace96f16bd9ab60135b60fd
Victoria <v.fakunle@alustudent.com>
2024-06-06 05:47:33 -0700

    help

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,4 @@
  #!/usr/bin/node
 const language = ['C is fun']
 let i = 0;
- while (i <= 10) {
-	 console.log(i);
-	 i++;
- } else { 
-	 console.log('Missing number of occurrences');
- }
-~         
+while (i
