3aca4c6bdb9135c0f7bf4a57f59751c451cacb56
Victoria Fakunle <v.fakunle@alustudent.com>
2024-06-06 15:05:07 +0200

    Update 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const language = ['C is fun']
-let i = 0;
- if (i < language.length) {
-	 console.log(language[i]);
-	 i++;
- } else { 
-	 console.log('Missing number of occurrences');
- }
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
