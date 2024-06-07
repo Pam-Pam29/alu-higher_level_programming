61ce140660486f3d0deaf09d34f0a1f2c42093d5
Victoria Fakunle <v.fakunle@alustudent.com>
2024-06-07 14:15:11 +0200

    Update 0-rectangle.js

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(height = 0, width = 0) { 
-    this.height = height;
-    this.width = width;
-  }
-}
-
-const r1 = new Rectangle(); 
-console.log(r1); 
+const Rectangle = class {
+};
+module.exports = Rectangle;
